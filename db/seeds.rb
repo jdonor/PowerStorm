b57386a56108c042155c12f0b6bb2e32c490b39d
unknown <mslyusarev14@.whitworth.edu>
2011-10-11 20:03:34 -0700

    work!!!

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+
+Building.create([{:building_name => "Arend", :occupants => 170},
+                 {:building_name => "Baldwin Jenkins", :occupants => 164},
+				 {:building_name => "Ballard", :occupants => 66},
+				 {:building_name => "Boppell", :occupants => 84},
+				 {:building_name => "Cornerstone", :occupants => 32},
+				 {:building_name => "Duvall", :occupants => 162},
+				 {:building_name => "East", :occupants => 170},
+				 {:building_name => "McMillan", :occupants => 85},
+				 {:building_name => "Stewart/Village", :occupants => 134},
+				 {:building_name => "Warren", :occupants => 227}])
+
+User.create(:name => "admin", :password => "password")
